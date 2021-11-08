76f9c501185c3dbd6f531ba9f85af084494d9f64
wujoannec <wu.joannec@gmail.com>
2021-11-07 23:57:35 -0800

    final

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,10 +54,23 @@ async function init() {
  * of installing it and getting it running
  */
 function initializeServiceWorker() {
+  /** Credit @ https://developers.google.com/web/fundamentals/primers/service-workers */
   /**
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
    */
+   if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+
 }
 
 /**
